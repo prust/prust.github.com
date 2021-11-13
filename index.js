34e8b30e59cdfceef09f4ead0ff0d71355e3650d
Peter Rust <peter@cornerstonenw.com>
2021-11-12 16:10:06 -0800

    Prettier

--- index.js
+++ index.js
@@ -1,56 +1,77 @@
-var fs = require('fs');
+var fs = require("fs");
 
-var marked = require('marked');
+var marked = require("marked");
 
-var months = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June', 'July', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.'];
+var months = [
+  "Jan.",
+  "Feb.",
+  "Mar.",
+  "Apr.",
+  "May",
+  "June",
+  "July",
+  "Aug.",
+  "Sep.",
+  "Oct.",
+  "Nov.",
+  "Dec.",
+];
 var index_snippets = [];
-var post_filenames = fs.readdirSync('_posts');
+var post_filenames = fs.readdirSync("_posts");
 post_filenames.reverse();
-post_filenames.forEach(function(filename) {
-  if (!filename.endsWith('.md'))
-    return;
+post_filenames.forEach(function (filename) {
+  if (!filename.endsWith(".md")) return;
 
-  var html_filename = filename.replace('.md', '.html');
-  var contents = fs.readFileSync('_posts/' + filename, {encoding: 'utf8'});
+  var html_filename = filename.replace(".md", ".html");
+  var contents = fs.readFileSync("_posts/" + filename, { encoding: "utf8" });
 
   // skip YML front-matter (marked off by --- before & after)
-  var first_ix = contents.indexOf('---');
-  var second_ix = contents.indexOf('---', first_ix + 1);
+  var first_ix = contents.indexOf("---");
+  var second_ix = contents.indexOf("---", first_ix + 1);
 
   var meta = {};
-  var yaml = contents.slice(first_ix + '---'.length, second_ix);
-  yaml.split('\n').forEach(function(line) {
-    var colon_ix = line.indexOf(':');
+  var yaml = contents.slice(first_ix + "---".length, second_ix);
+  yaml.split("\n").forEach(function (line) {
+    var colon_ix = line.indexOf(":");
     var key = line.slice(0, colon_ix).trim();
     var val = line.slice(colon_ix + 1).trim();
-    if (key && val)
-      meta[key] = val;
+    if (key && val) meta[key] = val;
   });
   var matches = /(\d\d\d\d)-(\d\d)-(\d\d)/.exec(filename);
   if (!matches)
-    throw new Error('Filename does not begin with YYYY-MM-DD: ' + filename);
+    throw new Error("Filename does not begin with YYYY-MM-DD: " + filename);
 
-  meta.date = months[parseInt(matches[2])-1] + ' ' + parseInt(matches[3]) + ', ' + matches[1];
-  meta.url = 'posts/' + html_filename;
+  meta.date =
+    months[parseInt(matches[2]) - 1] +
+    " " +
+    parseInt(matches[3]) +
+    ", " +
+    matches[1];
+  meta.url = "posts/" + html_filename;
 
-  var markdown = contents.slice(second_ix + '---'.length);
+  var markdown = contents.slice(second_ix + "---".length);
 
-  var markdown_parts = markdown.split('\n\n')
+  var markdown_parts = markdown.split("\n\n");
   var first_paragraph = markdown_parts[0];
-  var is_abbr = (markdown_parts[1] && markdown_parts[1].trim()) ||
+  var is_abbr =
+    (markdown_parts[1] && markdown_parts[1].trim()) ||
     (markdown_parts[2] && markdown_parts[2].trim());
-  index_snippets.push(genPostBody(meta, marked(first_paragraph), {abbr: is_abbr}));
-  var html = genPageTop(meta) +
+  index_snippets.push(
+    genPostBody(meta, marked(first_paragraph), { abbr: is_abbr })
+  );
+  var html =
+    genPageTop(meta) +
     genPostBody(meta, marked(markdown)) +
     genPageBottom(meta);
 
-  fs.writeFileSync('posts/' + html_filename, html);
+  fs.writeFileSync("posts/" + html_filename, html);
 });
 
-var index_html = genPageTop({title: "Peter's Dev Blog"}) +
-  index_snippets.join('') +
+var index_html =
+  genPageTop({ title: "Peter's Dev Blog" }) +
+  index_snippets.join("") +
   genPageBottom({});
-fs.writeFileSync('index.html', index_html);
+fs.writeFileSync("index.html", index_html);
 
 function genPageTop(meta) {
   return `
@@ -167,7 +188,11 @@ function genPostBody(meta, html, opts) {
     <div class="date">${meta.date}</div>
     <h1><a href=${meta.url}>${meta.title}</a></h1>
     ${html}
-    ${opts.abbr ? `<a href=${meta.url} class="more">Read More...</a><div style="clear: both"></div>` : ''}
+    ${
+      opts.abbr
+        ? `<a href=${meta.url} class="more">Read More...</a><div style="clear: both"></div>`
+        : ""
+    }
   </article>`;
 }
 
